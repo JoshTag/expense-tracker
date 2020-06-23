9d9f21e9d2a34c8244cfc955ac6e0a627a5af3e6
Joshua Taguicana <joshtaguicana@gmail.com>
2020-06-23 16:35:47 -0400

    completed backend for app.  Updated frontend to connect with backend

---
+++ README.md
@@ -0,0 +1,4 @@
+# Expense Tracker
+
+React expense tracker using React hooks, context api, express, and MongoDB
+
--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -1,21 +1,26 @@
-import React, { useContext } from 'react'
-import Transaction from "./Transaction"
+import React, { useContext, useEffect } from "react";
+import Transaction from "./Transaction";
 
-import { GlobalContext } from "./../context/GlobalState"
+import { GlobalContext } from "./../context/GlobalState";
 
 const TransactionList = () => {
-  const { transactions } = useContext(GlobalContext)
+  const { transactions, getTransactions } = useContext(GlobalContext);
+
+  useEffect(() => {
+    getTransactions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <>
-    <h3>History</h3>
-    <ul id="list" className="list">
-      {transactions.map(transaction => (
-        <Transaction key={transaction.id} transaction={transaction} />
-      ))}
-    </ul>
+      <h3>History</h3>
+      <ul id="list" className="list">
+        {transactions.map(transaction => (
+          <Transaction key={transaction._id} transaction={transaction} />
+        ))}
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default TransactionList
+export default TransactionList;
